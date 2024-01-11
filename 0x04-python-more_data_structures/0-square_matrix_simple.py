b44580661fc49992643a8c040aeabda7ff51f6a1
YvonneSilvia <yvonnesilvia111@gmail.com>
2024-01-11 03:09:49 -0800

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    square = []
+    for line in matrix:
+        square.append([c**2 for c in line])
+    return squared
+
+after creating each file, cat 0-main.py and copy the code they gave as an 0-mmain
+
